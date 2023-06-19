d06ac6bdb2450ef5824f08e069e1cc3522f43ea8
Ulpio <ulpionetto0@gmail.com>
2023-06-19 10:45:42 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,34 @@
+import {Header} from "../../components/Header"
+import background from "../../assets/background.png"
+import "./styles.css"
+import ItemList from "../../components/ItemList";
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <div className="conteudo">
+        <img src={background} alt="background" className="background"/>
+        <div className="info">
+          <h1>Encontre um perfil Github</h1>
+          <div>
+            <input name="usuario" placeholder="@username"/>
+            <button>Buscar</button>
+          </div>
+          <div className="perfil">
+            <img src="https://avatars.githubusercontent.com/u/98027343?s=96&v=4" alt="Profile" className="profile"/>
+            <div>
+              <h3>Ulpio Netto</h3>
+              <p>Descrição / Sobre mim</p>
+            </div>
+            <div>
+              <h4>Repostórios</h4>
+              <ItemList title="Nome do repositório" description="Descrição do repositório"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
